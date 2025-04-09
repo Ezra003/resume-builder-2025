98760574bc5a1edc80771ef3e889e5b8dbad1311
Ezra003 <126328199+Ezra003@users.noreply.github.com>
2025-04-09 08:47:13 +0200

    optimising for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -13,23 +13,16 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
   },
   experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
+    appDir: true,
   },
-  output: "export",
-  images: {
-    unoptimized: true,
+  webpack: (config) => {
+    config.resolve.fallback = { ...config.resolve.fallback, fs: false };
+    return config;
   },
 };
 
